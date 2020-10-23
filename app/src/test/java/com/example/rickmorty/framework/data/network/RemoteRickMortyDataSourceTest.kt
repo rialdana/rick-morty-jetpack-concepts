31c6ad8086ad7dbd716b4b7c55928501f1125844
Ricardo Cardona <rcardona@applaudostudios.com>
2020-10-23 12:23:21 -0600

    Created unit tests for RemoteRickMortyDataSource

---
+++ app/src/test/java/com/example/rickmorty/framework/data/network/RemoteRickMortyDataSourceTest.kt
@@ -0,0 +1,99 @@
+package com.example.rickmorty.framework.data.network
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.rickmorty.domain.Character
+import com.example.rickmorty.domain.CharactersResponse
+import com.example.rickmorty.framework.data.Result
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.anyInt
+import java.lang.RuntimeException
+
+@ExperimentalCoroutinesApi
+class RemoteRickMortyDataSourceTest {
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var mockApiService: RickMortyApiService
+    private lateinit var remoteRickMortyDataSource: RemoteRickMortyDataSource
+
+    @Before
+    fun setupApiService() {
+        mockApiService = Mockito.mock(RickMortyApiService::class.java)
+        remoteRickMortyDataSource = RemoteRickMortyDataSource(mockApiService)
+    }
+
+    @Test
+    fun getCharacters_throwError() = runBlockingTest {
+
+        val message = "Unable to fetch the characters"
+
+        `when`(mockApiService.getCharacters()).thenThrow(RuntimeException(message))
+
+        val result = remoteRickMortyDataSource.getCharacters()
+
+        result as Result.Error
+
+        assertThat(result.exception.message, `is`(message))
+    }
+
+    @Test
+    fun getCharacters_returnResponse() = runBlockingTest {
+        `when`(mockApiService.getCharacters()).thenReturn(CharactersResponse(null, emptyList()))
+
+        val result = remoteRickMortyDataSource.getCharacters()
+
+        result as Result.Success
+
+        assertThat(result.data.results, `is`(emptyList()))
+    }
+
+    @Test
+    fun getCharacterDetail_throwError() = runBlockingTest {
+        val message = "Unable to load character detail"
+        `when`(mockApiService.getCharacterDetail(ArgumentMatchers.anyInt())).thenThrow(
+            RuntimeException(message)
+        )
+
+        val result = remoteRickMortyDataSource.getCharacterDetail(ArgumentMatchers.anyInt())
+
+        result as Result.Error
+
+        assertThat(result.exception.message, `is`(message))
+    }
+
+    @Test
+    fun getCharacterDetail_returnResponse() = runBlockingTest {
+        `when`(mockApiService.getCharacterDetail(ArgumentMatchers.anyInt())).thenReturn(
+            Character(
+                1,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+            )
+        )
+
+        val result = remoteRickMortyDataSource.getCharacterDetail(ArgumentMatchers.anyInt())
+
+        result as Result.Success
+
+        assertThat(result.data.id, `is`(1))
+    }
+}
